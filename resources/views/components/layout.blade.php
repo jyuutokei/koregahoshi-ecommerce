c1b35bdd041b0c3abc2093a7e8099b9ef989d756
furantuturu <furantuturu@proton.me>
2026-06-15 21:12:29 +0800

    Fix title rendering and adjust layout structure in layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Cloth' }}</title>
+    <title>{{ $title }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
+    <div class="bg-layer"></div>
+    <x-nav />
     <main>
         {{ $slot }}
     </main>
