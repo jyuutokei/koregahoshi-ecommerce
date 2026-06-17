a777b7a21d3d7235355a9683c2c63e3d86d45b4a
furantuturu <furantuturu@proton.me>
2026-06-17 19:35:20 +0800

    Refactor pagination button component for improved layout and functionality

--- resources/views/components/buttons/pagination-btn.blade.php
+++ resources/views/components/buttons/pagination-btn.blade.php
@@ -1,5 +1,8 @@
-@props(['direction' => 'left'])
-
-<button type="button" class="p-2 border">
-    <x-icons.angle-arrow direction="{{ $direction }}" />
-</button>
+<div {{ $attributes->merge(['class' => "md:flex gap-3"]) }}>
+    <button type="button" class="p-2 border cursor-pointer">
+        <x-icons.angle-arrow />
+    </button>
+    <button type="button" class="p-2 border cursor-pointer">
+        <x-icons.angle-arrow direction="right" />
+    </button>
+</div>
