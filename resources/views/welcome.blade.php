db36d6b138ff75c9c90e7c00c5abb7d142ddf8a7
furantuturu <furantuturu@proton.me>
2026-06-17 19:34:09 +0800

    Update welcome page layout for improved user experience

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,15 +26,88 @@
                     src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
                     alt="Fashion model wearing new summer collection">
             </div>
-            <div class="flex row-start-2 place-items-end justify-between">
-                <a role="button" class="inline-flex items-center justify-between px-6 py-2 bg-[#d9d9d9] w-50 md:w-2xs">
+            <div class="flex row-start-2 place-items-end-safe justify-between">
+                <a role="button" class="inline-flex items-center justify-between px-6 py-2 bg-altgray w-50 md:w-2xs">
                     Go To Shop
                     <x-icons.arrow />
                 </a>
-                <div class="hidden my-1 ml-7 gap-3 md:flex">
-                    <x-buttons.pagination-btn />
-                    <x-buttons.pagination-btn direction="right" />
+                <x-buttons.pagination-btn class="hidden my-1 ml-7" />
+            </div>
+        </div>
+        <div class="mt-35">
+            <div class="flex justify-between items-end-safe">
+                <h2 class="font-extrabold text-6xl md:text-5xl lg:text-5xl xl:text-6xl">NEW <br> THIS WEEK</h2>
+                <span class="text-gray-500">See All</span>
+            </div>
+            <div class="flex items-center-safe gap-6 p-1">
+                <x-card class="max-h-85" />
+                <x-card class="max-h-85" />
+                <x-card class="max-h-85" />
+                <x-card class="max-h-85" />
+            </div>
+            <div class="mt-4 flex justify-center items-center">
+                <x-buttons.pagination-btn />
+            </div>
+        </div>
+        <div class="mt-35">
+            <div>
+                <h2 class="font-extrabold text-6xl md:text-5xl lg:text-5xl xl:text-6xl">XIV <br> COLLECTIONS <br> 23-24
+                </h2>
+            </div>
+            <div class="mt-15">
+                <div class="flex justify-between items-end">
+                    <div class="flex gap-10">
+                        <span>(ALL)</span>
+                        <span class="text-gray-500">Men</span>
+                        <span class="text-gray-500">Women</span>
+                        <span class="text-gray-500">Kids</span>
+                    </div>
+                    <div>
+                        <p class="text-gray-500">Less to More</p>
+                        <p class="text-gray-500">More to Less</p>
+                    </div>
                 </div>
+                <div class="divider"></div>
+                <div class="flex items-center-safe gap-10 p-1">
+                    <x-card class="max-h-100" />
+                    <x-card class="max-h-100" />
+                    <x-card class="max-h-100" />
+                </div>
+            </div>
+            <div class="flex justify-center mt-5">
+                <button class="flex flex-col justify-center items-center cursor-pointer">
+                    <p class="text-gray-500 -mb-2">More</p>
+                    <x-icons.angle-arrow direction="down" />
+                </button>
+            </div>
+        </div>
+        <div class="mt-35">
+            <div>
+                <h2 class="text-center text-6xl md:text-5xl lg:text-5xl xl:text-6xl">OUR APPROACH TO FASHION DESIGN</h2>
+                <p class="text-xl text-center max-w-2xl mx-auto mt-5 font-light">at elegant vogue , we blend creativity
+                    with
+                    craftsmanship
+                    to
+                    create
+                    fashion
+                    that
+                    transcends trends and
+                    stands the test
+                    of time each design is meticulously crafted, ensuring the highest quelity exqulsite finish</p>
+            </div>
+            <div class="mt-8 flex gap-6">
+                <img class="max-w-87.5 max-h-112.5 object-cover border"
+                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                    alt="Fashion model wearing new summer collection">
+                <img class="max-w-87.5 max-h-112.5 object-cover border"
+                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                    alt="Fashion model wearing new summer collection">
+                <img class="max-w-87.5 max-h-112.5 object-cover border"
+                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                    alt="Fashion model wearing new summer collection">
+                <img class="max-w-87.5 max-h-112.5 object-cover border"
+                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                    alt="Fashion model wearing new summer collection">
             </div>
         </div>
     </div>
