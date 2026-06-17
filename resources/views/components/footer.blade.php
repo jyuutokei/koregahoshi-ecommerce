5e219c16ccc8a9b9eea14e70b915359bce69c3b3
furantuturu <furantuturu@proton.me>
2026-06-17 19:34:36 +0800

    Add footer component with information and language options

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="relative bg-altgray p-60 mt-55">
+    <div>
+        <div>
+            <p class="text-gray-500 mb-5 font-light">INFO</p>
+            <ul>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+        </div>
+        <div class="mt-20">
+            <p class="text-gray-500 mb-5 font-light">LANGUAGES</p>
+            <ul>
+                <li>ENG</li>
+                <li>ESP</li>
+                <li>SVE</li>
+            </ul>
+        </div>
+    </div>
+    <p class="text-xs text-gray-500 absolute bottom-0 left-1/2 -translate-x-1/2">@2024 -- Copyright</p>
+</footer>
