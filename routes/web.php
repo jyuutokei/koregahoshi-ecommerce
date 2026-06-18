22200380f76b685d81711197715a53087d56f340
furantuturu <furantuturu@proton.me>
2026-06-18 21:24:17 +0800

    Add named routes for home and products pages

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', fn () => view('welcome'))->name('home');
+Route::get('/products', fn () => view('products'))->name('products');
