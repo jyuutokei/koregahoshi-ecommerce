7935b290390d35ad4243f9b522bc74f82c6babc7
furantuturu <furantuturu@proton.me>
2026-06-18 21:23:18 +0800

    Add size tabs component for product size selection

---
+++ resources/views/components/buttons/size-tabs.blade.php
@@ -0,0 +1,7 @@
+<div role="tablist" class="tabs tabs-box bg-transparent gap-2">
+    <a role="tab" class="tab tab-active">XS</a>
+    <a role="tab" class="tab border">S</a>
+    <a role="tab" class="tab">M</a>
+    <a role="tab" class="tab">L</a>
+    <a role="tab" class="tab">XL</a>
+</div>
