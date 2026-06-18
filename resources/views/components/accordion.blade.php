a4cd7eb6acf4a4a589d19afbc919f622143c0d47
furantuturu <furantuturu@proton.me>
2026-06-18 21:22:40 +0800

    Add accordion component for collapsible content display

---
+++ resources/views/components/accordion.blade.php
@@ -0,0 +1,4 @@
+<details class="collapse collapse-arrow border border-base-300">
+    <summary class="collapse-title font-semibold">{{ $slot }}</summary>
+    <div class="collapse-content text-sm"></div>
+</details>
