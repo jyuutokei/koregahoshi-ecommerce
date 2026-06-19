c08d83d0f5a6ef93ed049aeebcdab68fc5633a66
furantuturu <furantuturu@proton.me>
2026-06-19 23:17:49 +0800

    Remove border class from size tab for improved consistency

--- resources/views/components/buttons/size-tabs.blade.php
+++ resources/views/components/buttons/size-tabs.blade.php
@@ -1,6 +1,6 @@
 <div role="tablist" class="tabs tabs-box bg-transparent gap-2">
     <a role="tab" class="tab tab-active">XS</a>
-    <a role="tab" class="tab border">S</a>
+    <a role="tab" class="tab">S</a>
     <a role="tab" class="tab">M</a>
     <a role="tab" class="tab">L</a>
     <a role="tab" class="tab">XL</a>
