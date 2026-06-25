b8bdc230227f3ddb025d594e2eef299589749b35
furantuturu <furantuturu@proton.me>
2026-06-25 21:40:01 +0800

    Add route for cart view: implement route to display the cart page

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/', fn () => view('welcome'))->name('home');
 Route::get('/products', fn () => view('products'))->name('products');
 Route::get('/product', fn () => view('product'))->name('product');
 Route::get('/checkout', fn () => view('checkout'))->name('checkout');
+Route::get('/cart', fn () => view('cart'))->name('cart');
