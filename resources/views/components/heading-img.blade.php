c82be3776a9bdb3102622f89c41ec17a30831a60
furantuturu <furantuturu@proton.me>
2026-06-29 23:31:42 +0800

    Add heading image component for enhanced visual presentation

---
+++ resources/views/components/heading-img.blade.php
@@ -0,0 +1,5 @@
+<x-img3dhover>
+    <img class="w-full h-full object-cover object-center min-h-0"
+        src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+        alt="Fashion model wearing new summer collection">
+</x-img3dhover>
