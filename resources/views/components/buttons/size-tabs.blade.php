ca75f365a211d756c3dd1ce183105329a442540d
furantuturu <furantuturu@proton.me>
2026-06-29 23:33:00 +0800

    Add background transparency to size tabs for improved visual consistency

--- resources/views/components/buttons/size-tabs.blade.php
+++ resources/views/components/buttons/size-tabs.blade.php
@@ -1,4 +1,4 @@
-<div role="tablist" class="tabs tabs-box gap-2">
+<div role="tablist" class="tabs tabs-box gap-2 bg-transparent">
     <a role="tab" class="tab tab-active">XS</a>
     <a role="tab" class="tab">S</a>
     <a role="tab" class="tab">M</a>
