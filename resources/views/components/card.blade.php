87ed5c1fcab479d842bd9235763035b319291519
furantuturu <furantuturu@proton.me>
2026-06-29 23:31:33 +0800

    Refactor card component structure to improve layout and accessibility

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,17 +1,16 @@
-<div class="card rounded-none mt-7 overflow-hidden min-w-0">
-    <figure class="border relative">
-        <img {{ $attributes->merge(['class' => "w-full object-cover"]) }}
-            src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
-            alt="Fashion model wearing new summer collection">
-        <button class="absolute bottom-0 left-1/2 -translate-x-1/2 p-2 bg-altgray">
-            <x-icons.plus />
-        </button>
-    </figure>
-    <div class="card-body px-1">
-        <span class="text-gray-500">V-Neck T-Shirt</span>
-        <div class="flex justify-between">
-            <h3 class="card-title">Embroided Seersucker Shirt</h3>
-            <h3 class="card-title">$99</h3>
+<div class="card rounded-none min-w-0">
+    <a href="/exist">
+        <figure class="border relative">
+            <img {{ $attributes->merge(['class' => "w-full object-cover"]) }}
+                src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                alt="Fashion model wearing new summer collection">
+        </figure>
+        <div class="card-body px-1">
+            <span class="text-gray-500">V-Neck T-Shirt</span>
+            <div class="flex justify-between gap-10">
+                <h3 class="card-title truncate line-clamp-1">Embroided Seersucker Shirt</h3>
+                <h3 class="card-title">$99</h3>
+            </div>
         </div>
-    </div>
+    </a>
 </div>
