34e921c9433c07a3ecc84ff79a2664606c7d5064
furantuturu <furantuturu@proton.me>
2026-07-01 23:56:33 +0800

    Rename 'products' route to 'shop'

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'))->name('home');
-Route::get('/products', fn () => view('products'))->name('products');
-Route::get('/product', fn () => view('product'))->name('product');
-Route::get('/checkout', fn () => view('checkout'))->name('checkout');
-Route::get('/cart', fn () => view('cart'))->name('cart');
+Route::get('/', fn() => view('welcome'))->name('home');
+Route::get('/shop', fn() => view('shop'))->name('shop');
+Route::get('/product', fn() => view('product'))->name('product');
+Route::get('/checkout', fn() => view('checkout'))->name('checkout');
+Route::get('/cart', fn() => view('cart'))->name('cart');
