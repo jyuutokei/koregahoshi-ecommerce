2ae2e36f11bfea11a1806659f4f81be9480f5b9e
furantuturu <furantuturu@proton.me>
2026-07-02 21:20:09 +0800

    Refactor checkout layout for improved responsiveness and readability

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -6,7 +6,7 @@
     <div class="min-w-0">
         <h1 class="font-extrabold text-3xl">CHECKOUT</h1>
         <div class="mt-8">
-            <div class="flex gap-5">
+            <div class="flex flex-col md:flex-row md:gap-5">
                 <div class="flex-1 border py-4 px-7">
                     <h3 class="font-bold text-lg">Delivery Address</h3>
                     <p class="text-sm text-gray-500">Enter your address to see delivery options</p>
@@ -19,7 +19,7 @@
                                     mt-3 bg-altgray">Change</button>
                     </div>
                 </div>
-                <div class="divider lg:divider-horizontal"></div>
+                <div class="divider md:divider-horizontal"></div>
                 <div class="border py-4 px-7">
                     <h3 class="font-bold text-lg">Payment Method</h3>
                     <p class="font-light text-sm text-gray-500">Choose your payment method</p>
@@ -31,18 +31,18 @@
                 </div>
             </div>
         </div>
-        <div class="divider"></div>
+        <div class="divider mt-5"></div>
         <h3 class="font-bold text-2xl">Your Order</h3>
-        <div class="flex gap-5 mt-8">
+        <div class="flex flex-col md:flex-row gap-5 mt-8">
             <div class="grid grid-cols-[repeat(auto-fill,minmax(max(100%/3,250px),1fr))] gap-5 w-full">
                 <x-card-order />
                 <x-card-order />
                 <x-card-order />
                 <x-card-order />
             </div>
-            <div class="divider lg:divider-horizontal"></div>
+            <div class="divider md:divider-horizontal"></div>
             <div>
-                <div class="border p-6 w-sm sticky top-15">
+                <div class="border p-6 w-full md:w-sm sticky top-15">
                     <div class="flex justify-between items-center mb-2">
                         <p>Merchandise Subtotal:</p>
                         <p class="font-bold">$99</p>
